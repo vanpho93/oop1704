eabab1537ec09e783f5a0b9dfc424cbc55cfad04
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 19:41:44 +0700

    ob

--- a.js
+++ a.js
@@ -1,7 +1,14 @@
-const person = { name: 'Teo', age: 10 };
+const person = {
+    name: 'Teo',
+    age: 10,
+    school: { name: 'Khoa Pham', address: '92 LTR' },
+    sayHello: function() {
+        console.log('Hello, I am ' + this.name);
+    }
+};
+person.sayHello();
+// console.log(person.name);
+// console.log(person.height);
+// person.age = 11;
 
-console.log(person.name);
-console.log(person.height);
-person.age = 11;
-
-console.log(person);
+// console.log(person.school.name);
