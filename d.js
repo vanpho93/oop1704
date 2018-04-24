f7a8772879b33fd3011e86b463f2dd3f09468e86
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 20:29:44 +0700

    ref

---
+++ d.js
@@ -0,0 +1,15 @@
+// let a = 5;
+// let b = a;
+// a = 10;
+
+// console.log(b);
+
+// let person = { age: 5 };
+// let p2 = person;
+// person.age = 10;
+// console.log(p2);
+
+let teo1 = { name: 'teo' };
+let teo2 = { name: 'teo' };
+let teo3 = teo1;
+console.log(teo1 === teo3);
