807b9d497c58babc5a5b033210fcd0102c082de0
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 20:53:56 +0700

    array

---
+++ f.js
@@ -0,0 +1,8 @@
+// const arr = new Array();
+const arr = [1, 3, 5, 'chuoi'];
+
+console.log(arr.length);
+console.log(arr[0]);
+
+arr[2] = 10;
+console.log(arr);
---
+++ g.js
@@ -0,0 +1,10 @@
+const arr = ['NodeJS', 'Angular', 'Express', 'MEAN'];
+
+// arr.push('PHP');
+// arr.unshift('PHP');
+// arr.pop();
+// arr.shift();
+// arr.splice(1, 2);
+arr.splice(1, 0, 'Android', 'iOS');
+
+console.log(arr);
