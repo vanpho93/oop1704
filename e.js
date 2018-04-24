bc57eece6c61cd603276dfcbeb0a13092c615db7
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 20:41:16 +0700

    ...

--- e.js
+++ e.js
@@ -4,3 +4,14 @@ const { name, age, height } = person;
 // const age = person.age;
 // const height = person.height;
 console.log(name, age, height);
+
+// const arr = [1, 3, 5, 10, 11, 12];
+
+// const [a1, a2, a3, ...rest] = arr;
+
+// console.log(a1, a2, a3, rest);
+function showAge({ age }) {
+    console.log(age);
+}
+
+showAge({ age: 10 });
