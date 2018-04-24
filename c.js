744ef614f75a885a8abb9ee009ba3f64a2549cf6
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 20:24:20 +0700

    getter

--- c.js
+++ c.js
@@ -15,7 +15,7 @@ class Line {
         this.pointB = pointB;
     }
 
-    getLength() {
+    get length() {
         const dx = this.pointA.x - this.pointB.x;
         const dy = this.pointA.y - this.pointB.y;
         return Math.sqrt(dx * dx + dy * dy);
@@ -33,7 +33,7 @@ class Triangle {
         const l1 = new Line(this.pointA, this.pointB);
         const l2 = new Line(this.pointA, this.pointC);
         const l3 = new Line(this.pointC, this.pointB);
-        return l1.getLength() + l2.getLength() + l3.getLength();
+        return l1.length + l2.length + l3.length;
     }
 }
 
@@ -42,6 +42,6 @@ const pB = new Point(0, 1);
 const pC = new Point(0, 0);
 const lineAC = new Line(pA, pC);
 const tABC = new Triangle(pA, pB, pC);
-console.log(pB.getDistance());
-console.log(lineAC.getLength());
+// console.log(pB.getDistance());
+console.log(lineAC.length);
 console.log(tABC.getPerimeter());
