384494a042a6a7493c527d2e330644b5eeb94151
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 20:36:12 +0700

    x

---
+++ e.js
@@ -0,0 +1,6 @@
+const person = { name: 'Teo', age: 10, height: 100 };
+const { name, age, height } = person;
+// const name = person.name;
+// const age = person.age;
+// const height = person.height;
+console.log(name, age, height);
