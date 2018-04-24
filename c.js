8179a3123a8f40e31d1335c3b54ef17c3db659a8
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 20:21:52 +0700

    Triangle

--- c.js
+++ c.js
@@ -9,6 +9,39 @@ class Point {
     }
 }
 
-const pA = new Point(1, 1);
-const pB = new Point(0, 100);
+class Line {
+    constructor(pointA, pointB) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
+
+    getLength() {
+        const dx = this.pointA.x - this.pointB.x;
+        const dy = this.pointA.y - this.pointB.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+class Triangle {
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+    }
+
+    getPerimeter() {
+        const l1 = new Line(this.pointA, this.pointB);
+        const l2 = new Line(this.pointA, this.pointC);
+        const l3 = new Line(this.pointC, this.pointB);
+        return l1.getLength() + l2.getLength() + l3.getLength();
+    }
+}
+
+const pA = new Point(1, 0);
+const pB = new Point(0, 1);
+const pC = new Point(0, 0);
+const lineAC = new Line(pA, pC);
+const tABC = new Triangle(pA, pB, pC);
 console.log(pB.getDistance());
+console.log(lineAC.getLength());
+console.log(tABC.getPerimeter());
